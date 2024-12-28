48ba1f48d90d54f1a50ee931fb9e4fed624ed4dd
unknown <celmarortiz98@yahoo.com>
2024-12-28 13:19:52 +0800

    add 768 responsive

--- src/common/components/carousel/slider.tsx
+++ src/common/components/carousel/slider.tsx
@@ -1,5 +1,6 @@
 import { translate } from "../../i18n";
 import { ImageBackgroundInterface } from "../../models";
+import "./carousel.css";
 
 interface SliderProps {
   index: number;
@@ -11,18 +12,8 @@ export const SliderView = (props: SliderProps) => {
   const { data, index, onImageLoad } = props;
   return (
     <div
-      style={{
-        width: "300px",
-        height: "100%",
-        backgroundImage: `url(${data.stringContent})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        borderRadius: 20,
-        zIndex: -1,
-        display: "flex",
-        alignItems: "end",
-        boxShadow: "10px 5px 15px rgba(0, 0, 0, 0.7)",
-      }}
+      className="custom-slider-item "
+      style={{ backgroundImage: `url(${data.stringContent})` }}
     >
       <img
         src={data.stringContent}
@@ -32,34 +23,11 @@ export const SliderView = (props: SliderProps) => {
           display: "none", // Hide the img tag
         }}
       />
-      <div
-        style={{
-          width: "100%",
-          height: "35%",
-          paddingLeft: "10%",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <span
-          style={{
-            height: "20%",
-            fontFamily: "DM Sans, sans-serif",
-            fontSize: "17px",
-            color: "rgba(255, 255, 255, 0.8)",
-          }}
-        >
+      <div className="custom-container ">
+        <span className="location-text">
           {translate("home.location", { location: data.location })}
         </span>
-        <span
-          style={{
-            height: "80%",
-            fontFamily: "DM Sans, sans-serif",
-            fontSize: "28px",
-            color: "rgba(255, 255, 255)",
-            whiteSpace: "pre-line",
-          }}
-        >
+        <span className="name-text">
           {translate("home.name", { name: data.name })}
         </span>
       </div>
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -12,96 +12,29 @@ export const HomePage = observer(() => {
     appStateStore.setLoading(true);
   }, []);
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          height: "50%",
-          width: "100%",
-          display: "flex",
-        }}
-      >
-        <div
-          style={{
-            height: "100%",
-            width: "40%",
-            paddingLeft: "2%",
-            paddingRight: "1%",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              paddingLeft: 5,
-              alignItems: "end",
-              color: "#ffffff",
-              fontFamily: "Inter, sans-serif",
-              fontSize: "30px",
-              textShadow: "2px 8px 4px rgba(0, 0, 0, 0.7)", // Add text shadow
-            }}
-          >
+    <div className="main-container">
+      <div className="sub-container">
+        <div className="left-container">
+          <div className="location-text-style ">
             {translate("home.location", {
               location: backgroundImageStore.location,
             })}
           </div>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              alignItems: "end",
-              color: "#ffffff",
-              fontFamily: "DM Sans, sans-serif",
-              fontSize: "90px",
-              textShadow: "2px 5px 4px rgba(0, 0, 0, 0.7)", // Add text shadow
-            }}
-          >
+          <div className="name-text-style ">
             {translate("home.name", {
               name: backgroundImageStore.name,
             })}
           </div>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              paddingLeft: 5,
-              alignItems: "start",
-              color: "#ffffff",
-              fontFamily: "Inter, sans-serif",
-              fontSize: "25px",
-              textShadow: "2px 8px 4px rgba(0, 0, 0, 0.7)", // Add text shadow
-            }}
-          >
+          <div className="desc-text-style">
             {translate("home.description", {
               description: backgroundImageStore.description,
             })}
           </div>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              marginTop: "5%",
-              paddingLeft: 5,
-              alignItems: "center",
-            }}
-          >
+          <div className="explore-flex-style">
             <Button className="explore">{translate("home.explore")}</Button>
           </div>
         </div>
-        <div
-          style={{
-            height: "100%",
-            width: "60%",
-          }}
-        >
+        <div className="right-container ">
           <InfiniteSlider />
         </div>
       </div>
